f5ec1200b8e365efc3d1349b27e24b633362f974
Анастасия <nastena.andreevna.99@gmail.com>
2017-10-04 23:30:53 +0300

    Update Map.h

--- KR -Model of Road Traffic/Map.h
+++ KR -Model of Road Traffic/Map.h
@@ -6,58 +6,62 @@ class Map
 	sf::Texture Texture;
 	sf::Sprite Sprite;
 	std::vector<std::string> TileMap;
+	int Height;
+	int Weight;
 public:
 	Map(const sf::String file)
 	{
 		Texture.loadFromFile("images/" + file);
 		Sprite.setTexture(Texture);
+		Height = 48;
+		Weight = 80;
 		TileMap = 
 		{
 			"01111111111111111111111111111111111111111111111111111111111111111111111111111110",
 			"0rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr0",
 			"0rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr0",
-			"0rr     h     s                rr       rr       s          rr      h   s    rr0",
-			"0rr     rr             s       rr    s  rrrrrrrr       s    rr rrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrr            rr       rrrrrrrr    h       rrrrrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrr    rrrrrrrrrr             rrrrrrrrrrrrrrrr      h        rr0",
-			"0rr            s rr    rrsrrrrrrr       h     rrrrrrrrrrrrrrrr         s     rr0",
-			"0rr    s         rrrrrrrr      rrrrrrrrrr  s  rr             h   rrrrrrrrrrrrrr0",
-			"0rr              rrrrrrrr s    rrrrrrrrrr     rr     h           rrrrrrrrrrrrrr0",
-			"0rr     s  s     rr                    rr     rr        s        rr          rr0",
-			"0rr   s          rr s       h     s    rr     rr                 rr    h     rr0",
-			"0rr          rrrrrrrrrrrr       rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr          rr0",
-			"0rr        s rrrrrrrrrrrr     s rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr          rr0",
-			"0rr  h       rr        rr       rr     rr             s      rr     s        rr0",
-			"0rr          rr        rrrrrrrrrrr     rr        h           rrrrrrrrr       rr0",
-			"0rrrrrrrrrrrrrr        rrrrrrrrrrr     rrrrrrrrrr   s        rrrrrrrrr    h  rr0",
-			"0rrrrrrrrrrrrrr     s        s  rr     rrrrrrrrrr      rrrrrrrr     rr       rr0",
-			"0rr       s       rrrrrrrrrrrrrrrr    s        rr      rrrrrrrr   s rr       rr0",
-			"0rr   h           rrrrrrrrrrrrrrrr       h     rr      rr    rrrrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrrr                   s       rr      rr    rrrrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrrr        h             s    rr      rr             rr     rr0",
-			"0rr     h    s    rr           s     h         rr      rr                    rr0",
-			"0rr               rr                           rrrrrrrrrr              h     rr0",
-			"0rr                                                rr                        rr0",
-			"0rr     rr             s       rr    s  rrrrrrrr       s    rr rrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrr            rr       rrrrrrrr    h       rrrrrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrr    rrrrrrrrrr             rrrrrrrrrrrrrrrr      h        rr0",
-			"0rr            s rr    rrsrrrrrrr       h     rrrrrrrrrrrrrrrr         s     rr0",
-			"0rr    s         rrrrrrrr      rrrrrrrrrr  s  rr             h   rrrrrrrrrrrrrr0",
-			"0rr              rrrrrrrr s    rrrrrrrrrr     rr     h           rrrrrrrrrrrrrr0",
-			"0rr     s  s     rr                    rr     rr        s        rr          rr0",
-			"0rr   s          rr s       h     s    rr     rr                 rr    h     rr0",
-			"0rr          rrrrrrrrrrrr       rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr          rr0",
-			"0rr        s rrrrrrrrrrrr     s rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr          rr0",
-			"0rr  h       rr        rr       rr     rr             s      rr     s        rr0",
-			"0rr          rr        rrrrrrrrrrr     rr        h           rrrrrrrrr       rr0",
-			"0rrrrrrrrrrrrrr        rrrrrrrrrrr     rrrrrrrrrr   s        rrrrrrrrr    h  rr0",
-			"0rrrrrrrrrrrrrr     s        s  rr     rrrrrrrrrr      rrrrrrrr     rr       rr0",
-			"0rr       s       rrrrrrrrrrrrrrrr    s        rr      rrrrrrrr   s rr       rr0",
-			"0rr   h           rrrrrrrrrrrrrrrr       h     rr      rr    rrrrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrrr                   s       rr      rr    rrrrrrrrrrrrrrrrrr0",
-			"0rrrrrrrrrrrrrrrrrrr        h             s    rr      rr             rr     rr0",
-			"0rr     h    s    rr           s     h         rr      rr                    rr0",
-			"0rr               rr                           rrrrrrrrrr              h     rr0",
+			"0rr           f                rr       rr       f          rr          f    rr0",
+			"0rr                            rr    f  rrrrrrrr   hl  f    rrrrrrrrrrrrrrrrrrr0",
+			"0rr  hlrrrrrrrrrrrr            rr  hl   rrrrrrrr   mn       rrrrrrrrrrrrrrrrrrr0",
+			"0rr  mnrrrrrrrrrrrr    rrrrrrrrrr  mn         rrrrrrrrrrrrrrrr      hl       rr0",
+			"0rr            f rr    rrsrrrrrrr             rrrrrrrrrrrrrrrr      mn f     rr0",
+			"0rr    f         rrrrrrrr      rrrrrrrrrr  f  rr             h   rrrrrrrrrrrrrr0",
+			"0rr              rrrrrrrr f    rrrrrrrrrr     rr     hl          rrrrrrrrrrrrrr0",
+			"0rr     f  f     rr        hl          rr     rr     mn f        rr          rr0",
+			"0rr   f          rr f      mn     f    rr     rr                 rr    hl    rr0",
+			"0rr          rrrrrrrrrrrr       rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr    mn    rr0",
+			"0rr        f rrrrrrrrrrrr     f rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr          rr0",
+			"0rr          rr        rr       rr     rr           hl f     rr     f        rr0",
+			"0rr          rr        rrrrrrrrrrr     rr           mn       rrrrrrrrr   hl  rr0",
+			"0rrrrrrrrrrrrrr        rrrrrrrrrrr     rrrrrrrrrr            rrrrrrrrr   mn  rr0",
+			"0rrrrrrrrrrrrrr     f        f  rr     rrrrrrrrrr      rrrrrrrr     rr       rr0",
+			"0rr       f       rrrrrrrrrrrrrrrr    f        rr      rrrrrrrr   f rr       rr0",
+			"0rr               rrrrrrrrrrrrrrrr         hl  rr    f rr    rrrrrrrrrrrrrrrrrr0",
+			"0rrrrrrrrrrrrrrrrrrr                   f   mn  rr      rr    rrrrrrrrrrrrrrrrrr0",
+			"0rrrrrrrrrrrrrrrrrrr       hl             f    rr      rr                    rr0",
+			"0rr          f    rr       mn  f    hl   rrrrrrrrrrrrrrrr     f       hl     rr0",
+			"0rr    hl     rrrrrrrrrrr           mn   rrrrrrrrrrrrrrrr             mn     rr0",
+			"0rr    mn     rrrrrrrrrrr                rr                                  rr0",
+			"0rr           rr       rr      rrrrrrrrrrrrrrrrr       f    rrrrrrrrrrrrrrrrrrr0",
+			"0rrrrrrrrrrrrrrrrrr    rr      rrrrrrrrrrrrrrrrr            rrrrrrrrrrrrrrrrrrr0",
+			"0rrrrrrrrrrrrrrrrrr    rrrrrrrrrr             rrrrrrrrrrrrrrrr               rr0",
+			"0rr            f rr    rrsrrrrrrr             rrrrrrrrrrrrrrrr         f     rr0",
+			"0rr    f         rrrrrrrr      rrrrrrrrrr  f  rr                 rrrrrrrrrrrrrr0",
+			"0rr              rrrrrrrr f    rrrrrrrrrr     rr     hl          rrrrrrrrrrrrrr0",
+			"0rr     f  f     rr                    rr     rr     mn f        rr        f rr0",
+			"0rr   f          rr f             f    rr     rr                 rr    hl    rr0",
+			"0rr          rrrrrrrrrrrr       rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr    mn    rr0",
+			"0rr   hl   f rrrrrrrrrrrr     f rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr          rr0",
+			"0rr   mn     rr        rr       rr     rr   f         f      rr     f        rr0",
+			"0rr          rr        rrrrrrrrrrr     rr                    rrrrrrrrrr  hl  rr0",
+			"0rrrrrrrrrrrrrr        rrrrrrrrrrr     rrrrrrrrrr   f        rrrrrrrrrr  mn  rr0",
+			"0rrrrrrrrrrrrrr     f        f  rr     rrrrrrrrrr      rrrrrrrr     rr       rr0",
+			"0rr       f       rrrrrrrrrrrrrrrr    f        rr      rrrrrrrr   f rr       rr0",
+			"0rr               rrrrrrrrrrrrrrrr    hl       rr      rr    rrrrrrrrrrrrrrrrrr0",
+			"0rrrrrrrrrrrrrrrrrrr             f    mn       rr   f  rr    rrrrrrrrrrrrrrrrrr0",
+			"0rrrrrrrrrrrrrrrrrrr       hl             f    rr      rr    rr       rr     rr0",
+			"0rr          f    rr       mn  f       f       rr      rr             hl     rr0",
+			"0rr   f           rr                           rr      rr             mn     rr0",
 			"0rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr0",
 			"0rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr0",
 			"01111111111111111111111111111111111111111111111111111111111111111111111111111110",
@@ -66,44 +70,64 @@ public:
 
 	void draw(sf::RenderWindow & window)
 	{
-		for (int i = 0; i < 48; ++i)
+		for (int i = 0; i < Height; ++i)
 		{
-			for (int j = 0; j < 80; ++j)
+			for (int j = 0; j < Weight; ++j)
 			{
 				switch (TileMap[i][j])
 				{
 					case '0':
 					{
-						Sprite.setTextureRect(sf::IntRect(10, 0, 8, 8));
+						Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));
 						break;
 					}
 					case 'r':
 					{
-						Sprite.setTextureRect(sf::IntRect(100, 0, 8, 8)); 
+						Sprite.setTextureRect(sf::IntRect(120, 0, 120, 120)); 
 						break;
 					}
-					case 's':
+					case 'f':
 					{
-						Sprite.setTextureRect(sf::IntRect(310, 150, 8, 8));
+						Sprite.setTextureRect(sf::IntRect(0, 120, 120, 120));
 						break;
 					}
 					case 'h':
 					{
-						Sprite.setTextureRect(sf::IntRect(1185, 129, 8, 8));
+						Sprite.setTextureRect(sf::IntRect(360, 120, 120, 120));
 						break;
+					}
+					case 'l':
+					{
+								Sprite.setTextureRect(sf::IntRect(480, 120, 120, 120));
+								break;
+					}
+					case 'm':
+					{
+								Sprite.setTextureRect(sf::IntRect(360, 240, 120, 120));
+								break;
+					}
+					case 'n':
+					{
+								Sprite.setTextureRect(sf::IntRect(480, 240, 120, 120));
+								break;
 					}
 					case ' ':
 					{
-						Sprite.setTextureRect(sf::IntRect(550, 0, 8, 8));
+						Sprite.setTextureRect(sf::IntRect(240, 0, 120, 120));
 						break;
 					}
 					case '1':
 					{
-						Sprite.setTextureRect(sf::IntRect(10, 0, 8, 8));
+						Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));
 						break;
 					}
+					case 'c':
+					{
+							Sprite.setTextureRect(sf::IntRect(120, 0, 120, 120));
+							break;
+					}
 				}
-				Sprite.setPosition(j * 8, i * 8);
+				Sprite.setPosition(j * 120, i * 120);
 				window.draw(Sprite);
 			}
 		}
@@ -112,6 +136,22 @@ public:
 	~Map()
 	{
 		TileMap.clear();
+	}
+
+
+	std::vector<std::string> & getTM()
+	{
+		return TileMap;
+	}
+
+	const int getH()
+	{
+		return Height;
+	}
+
+	const int getW()
+	{
+		return Weight;
 	}
 };
 
