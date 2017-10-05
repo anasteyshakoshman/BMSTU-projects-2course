7131cf868c97abb417b92ed4fccc7e62c5491eb8
Анастасия <nastena.andreevna.99@gmail.com>
2017-10-05 23:57:05 +0300

    Update Car.cpp

--- KR -Model of Road Traffic/Car.cpp
+++ KR -Model of Road Traffic/Car.cpp
@@ -3,21 +3,23 @@
 #include "Map.h"
 
 
-Car::Car(const sf::String F, Map & map)
+Car::Car(const sf::Color col, Map & map)
 {
-	Image.loadFromFile("images/" + F);
+	Image.loadFromFile("images/car0.png");
+	Image.createMaskFromColor(sf::Color::White);
 	Texture.loadFromImage(Image);
 	Sprite.setTexture(Texture);
-	Sprite.setTextureRect(sf::IntRect(0, 0, 120, 100));
-	map.getTM()[6][7] = 'c';
-	X = 840;
-	Y = 720;
+	Sprite.setColor(col);
+	Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));
+	X = 7;
+	Y = 6;
+	//map.getTM()[Y][X] = 'c';
 	dX = 0;
 	dY = 0;
-	Speed = 0;
-	Direction = 1; // 0 (+X), 1 (+Y), 2(-X), 3(-Y)
-	Oil = 10.0;
-	Sprite.setPosition(X, Y);
+	//Speed = 0;
+	Direction = 0; // 0 (+X), 1 (+Y), 2(-X), 3(-Y)
+	Oil = 10;
+	Sprite.setPosition(120 * X, 120 * Y);
 }
 
 const sf::Sprite & Car::getSprite()
@@ -30,105 +32,80 @@ Car::~Car()
 {
 	X = 0;
 	Y = 0;
-	W = 0;
-	H = 0;
 	dX = 0;
 	dY = 0;
-	Speed  = 0;
+	//Speed  = 0;
 	Direction = 0;
 	Oil = 0;
 }
 
-bool Car::around(Map & map)
+void Car::coordinateChange(const std::vector<int> vec, const int index)
 {
-	if (map.getTM()[X + dX][Y + dY] == 'r' && map.getTM()[X + 2 * dX][Y + 2 * dY]) return true;
-	else return false;
+	if (!vec[index])
+	{
+		dX = 1;
+		dY = 0;
+		Image.loadFromFile("images/car0.png");
+	}
+	if (vec[index] == 1)
+	{
+		dX = 0;
+		dY = 1;
+		Image.loadFromFile("images/car1.png");
+	}
+	if (vec[index] == 2)
+	{
+		dX = -1;
+		dY = 0;	
+		Image.loadFromFile("images/car2.png");
+	}
+	if (vec[index] == 3)
+	{
+		dX = 0;
+		dY = -1;
+		Image.loadFromFile("images/car3.png");
+	}
+	Direction = vec[index];
+	Image.createMaskFromColor(sf::Color::White);
+	Texture.loadFromImage(Image);
+	Sprite.setTexture(Texture);
+	Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));
 }
 
-void Car::go(Map & map)
+
+std::vector<int> Car::freeeDirections(Map & map)
 {
-	int random = 0;
-	switch (Direction)
+	std::vector<int> vec;
+	if (Oil)
 	{
-	case 0:
-	{
-			  do
-			  {
-				  random = rand() % 3;
-				  if (!random) dX = 120;
-				  if (random == 1) dY = 120;
-				  else dY = -120;
-			  } 
-			  while (!around(map));
-			  break;
+		if (map.getTM()[Y][X + 1] == 'r' && map.getTM()[Y][X + 2] == 'r' && Direction != 2) vec.push_back(0);
+		if (map.getTM()[Y + 1][X] == 'r' && map.getTM()[Y + 2][X] == 'r' && Direction != 3) vec.push_back(1);
+		if (map.getTM()[Y][X - 1] == 'r' && map.getTM()[Y][X - 2] == 'r' && Direction != 0) vec.push_back(2);
+		if (map.getTM()[Y - 1][X] == 'r' && map.getTM()[Y - 2][X] == 'r' && Direction != 1) vec.push_back(3);
 	}	
-	case 1:
-	{
-			  do
-			  {
-				  random = rand() % 3;
-				  if (!random) dY = 120;
-				  if (random == 1) dX = 120;	
-				  else  dX = -120;
-			  } 
-			  while (!around(map));
-			  break;
+	return vec;
+}
 
-	}		
-	case 2:
-	{
-			  do
-			  {
-				  random = rand() % 3;
-				  if (!random) dY = -120;
-				  if (random == 1) dX = -120;
-				  else  dX = 120;
-			  } 
-			  while (!around(map));
-			  break;
 
-	}		
-	case 3:
-	{
-			  do
-			  {
-				  random = rand() % 3;
-				  if (!random) dX = -120;
-				  if (random == 1) dY = 120;
-				  else dY = -120;
-			  } 
-			  while (around(map));			 
-			  break;
 
-		}
+void Car::go(Map & map, sf::RenderWindow & window)
+{
+	auto freeDir(freeeDirections(map));
+	if (freeDir.size())
+	{
+		int random = rand() % freeDir.size();
+		for (int i = 0; i < freeDir.size(); ++i)
+		{
+			if (random == i) coordinateChange(freeDir, i);
+		}		
+		map.getTM()[Y][X] = 'r';
 		X += dX;
 		Y += dY;
-		if (dX == 120)
-		{
-			Direction = 0;
-			Sprite.setScale(1., 0.);
-		}
-		if (dX == -120)
-		{
-			Direction = 2;
-			Sprite.setScale(-1., 0.);
-		}
-		if (dY == 120)
-		{
-			Direction = 1;
-			Sprite.setScale(0., 1.);
-		}
-		if (dY == -120)
-		{
-			Direction = 3;
-			Sprite.setScale(0., -1.);
-		}
-		Sprite.setPosition(X, Y);
-		dX = 0; 
+		//map.getTM()[Y][X] = 'c';
+		Sprite.move(120 * dX, 120 * dY);
+		window.draw(Sprite);
+		//Oil -= 01;
+		dX = 0;
 		dY = 0;
-	}
-	
-
-
-	
+	}	
 }
