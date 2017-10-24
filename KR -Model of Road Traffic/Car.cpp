2cead1550d0120208894596affe7703d0a0df76e
Анастасия <nastena.andreevna.99@gmail.com>
2017-10-24 22:31:45 +0300

    Update Car.cpp

--- KR -Model of Road Traffic/Car.cpp
+++ KR -Model of Road Traffic/Car.cpp
@@ -1,56 +1,37 @@
 
 #include "Car.h"
-#include "Map.h"
 #include <math.h>
 #include <ctime>
+#include "Map.h"
+#include "TrafficLight.h"
+
+
 
 std::vector<Car> Car::AllCars;
 
-//Car::Car(const sf::Color col, Map & map)
-//{
-//	Image.loadFromFile("images/car0.png");     
-//	Image.createMaskFromColor(sf::Color::White);
-//	Texture.loadFromImage(Image);      
-//	Sprite.setTexture(Texture);        
-//	Sprite.setColor(col);              
-//	Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));  
-//	X = 7; 
-//	Y = 6;
-//	Speed = 0.05;
-//	dX = Speed;
-//	dY = 0;
-//	Direction = 0; // 0 (+X), 1 (+Y), 2(-X), 3(-Y)
-//	Oil = 100;
-//	ChangeDir = false;
-//	Sprite.setPosition(120 * X, 120 * Y);          
-//	AllCars.push_back(*this);
-//}
-
-Car::Car(const sf::Color col, Map & map, const double x, const double y)
+Car::Car(const sf::Color col, const Map &  map, sf::RenderWindow & window, const int x, const int y)
 {
-
 	Image.loadFromFile("images/car0.png");
 	Image.createMaskFromColor(sf::Color::White);
 	Texture.loadFromImage(Image);
 	Sprite.setTexture(Texture);
 	Sprite.setColor(col);
 	Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));
-	X = x;
-	Y = y;
-	Speed = 0.05;
+	X = x * 120;
+	Y = y * 120;
+	Speed = 0.05 * 120;
 	dX = Speed;
 	dY = 0;
 	Direction = 0; // 0 (+X), 1 (+Y), 2(-X), 3(-Y)
 	Oil = 100;
+	//TLMap = map;
 	ChangeDir = false;
-	Sprite.setPosition(120 * X, 120 * Y);
+	Sprite.setPosition(X, Y);
 	AllCars.push_back(*this);
-}
+	window.draw(Sprite);
+};
 
-const sf::Sprite & Car::getSprite()
-{
-	return Sprite;
-}
+
 
 
 Car::~Car()
@@ -62,56 +43,60 @@ Car::~Car()
 	Speed  = 0;
 	Direction = 0;
 	Oil = 0;
-}
+};
 
 
-const double Car::getX()
+const int Car::getX() const
 {
 	return X;
-}
-const double Car::getY()
+};
+
+const int Car::getY() const
 {
 	return Y;
-}
+};
 
 
-const bool Car::nextCar(const double dx, const double dy)
+const bool Car::nextCar(const int dx, const int dy, const Map & TLMap) const
 {
-	for (int i = 0; i < AllCars.size(); ++i)
+	int interval = 2;
+	if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p' && TLMap.getTM()[mY() + 2 * mdY()][mX() + 2 * mdX()] == 'p' /*\
+		|| TLMap.getTM()[*/ ) interval *= 10;
+	for (auto it = AllCars.begin(); it != AllCars.end(); ++it)
 	{
-		if (X + 4 * dx == AllCars[i].X  &&  Y + 4 * dy == Car::AllCars[i].Y) return false;
+		if (X + dx * interval + 120 == it->getX() && Y + dy * interval + 120 == it->getY()) return false;
 	}
 	return true;
-}
+};
 
 
-bool Car::coordinateChange(const std::vector<int> vec, const int index)
+bool Car::coordinateChange(const std::vector<int> vec, const int index, const Map & map)
 {
 	bool change = false;
 	if (Direction != vec[index])
 	{
-		if (!vec[index] && nextCar(Speed, 0))
+		if (!vec[index] && nextCar(Speed, 0, map))
 		{
 			dX = Speed;
 			dY = 0;
 			change = true;
 			Image.loadFromFile("images/car0.png");
 		}
-		if (vec[index] == 1 && nextCar(0, Speed))
+		if (vec[index] == 1 && nextCar(0, Speed, map))
 		{
 			dX = 0;
 			dY = Speed;
 			change = true;
 			Image.loadFromFile("images/car1.png");
 		}
-		if (vec[index] == 2 && nextCar(-Speed, 0))
+		if (vec[index] == 2 && nextCar(-Speed, 0, map))
 		{
 			dX = -Speed;
 			dY = 0;
 			change = true;
 			Image.loadFromFile("images/car2.png");
 		}
-		if (vec[index] == 3 && nextCar(0, -Speed))
+		if (vec[index] == 3 && nextCar(0, -Speed, map))
 		{
 			dX = 0;
 			dY = -Speed;
@@ -129,83 +114,137 @@ bool Car::coordinateChange(const std::vector<int> vec, const int index)
 		}
 		return change;
 	}	
-	else return nextCar(dX, dY);
+	else return nextCar(dX, dY, map);
+};
 
-}
-
-void Car::okruglenie()
+const int Car::mX() const
 {
-	X = round(X * 100) / 100.;
-	Y = round(Y * 100) / 100.;
-}
+	int x = X;
+	if (x % 120 == 0) return x / 120;
+	else
+	{
+		int ost = x;
+		while (ost > 120) ost -= 120;
+		if (Direction == 0 || Direction == 1) return (x - ost) / 120;   //floor
+		if (Direction == 2 || Direction == 3) return (x - ost + 120) / 120;   //ceil
+	}
+};
+
+const int Car::mY() const
+{
+	int y = Y;
+	if (y % 120 == 0) return y / 120;
+	else
+	{
+		int ost = y;
+		while (ost > 120) ost -= 120;
+		if (Direction == 0 || Direction == 1) return (y - ost) / 120;   //floor
+		if (Direction == 2 || Direction == 3) return (y - ost + 120) / 120;   //ceil
+	}
+};
+
+const int Car::mdX() const
+{
+	return dX / 6;
+};
+
+const int Car::mdY() const
+{
+	return dY / 6;
+};
 
 
-const std::vector<int> Car::freeDirections(Map & map)
+
+const std::vector<int> Car::freeDirections(const Map &  TLMap) const
 {
 	std::vector<int> vec;
-	if (Oil)
+
+//	if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'r' || \
+//		TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p' && \
+//(TLMap.getTM()[mY() + 2 * mdY()][mX() + 2 * mdX()] == 'p' || \
+//		TLMap.getTM()[mY() + 2 * mdY()][mX() + 2 * mdX()] == 'r' || \
+//		TLMap.getTM()[mY() + mdY() - 2 * mdX() * pow(-1, Direction)][mX() + mdX() - 2 * mdY() * pow(-1, Direction)] == 'r'))\
+//		vec.push_back(Direction);
+//
+//	if (TLMap.getTM()[mY() + mdX() * pow(-1, Direction)][mX() + mdY() * pow(-1, Direction)] == 'r' && \
+//		TLMap.getTM()[mY() + mdY() + mdX()][mX() + mdX() * pow(-1, Direction) + mdY() * pow(-1, Direction)] == 'r' && !ChangeDir)
+//		vec.push_back((Direction + 1) % 4);
+//
+//	if (TLMap.getTM()[mY() - 2 * mdX() * pow(-1, Direction)][mX() - 2 * mdY() * pow(-1, Direction)] == 'r' &&\
+//		TLMap.getTM()[mY() + mdY() - 2 * mdX() * pow(-1, Direction)][mX() + mdX() - 2 * mdY() * pow(-1, Direction)] == 'r'&& !ChangeDir)
+//		vec.push_back((Direction + 3) % 4);
+
+	                                   								
+	switch (Direction)                     
 	{
-		switch (Direction)
+		case 0:
 		{
-		    case 0:
-            {
-				  if (map.getTM()[floor(Y)][floor(X) + 1] == 'r' || \
-					  map.getTM()[floor(Y)][floor(X) + 1] == 'p' && (map.getTM()[floor(Y)][floor(X) + 2] == 'p' ||\
-					  map.getTM()[floor(Y)][floor(X) + 2] == 'r' ||  map.getTM()[floor(Y) - 2][floor(X) + 1] == 'r')) \
-						  vec.push_back(0);
-					  if (map.getTM()[floor(Y) + 1][floor(X)] == 'r' && map.getTM()[floor(Y) + 1][floor(X) + 1] == 'r' && !ChangeDir) vec.push_back(1);
-					  if (map.getTM()[floor(Y) - 2][floor(X)] == 'r' && map.getTM()[floor(Y) - 2][floor(X) - 1] == 'r'&& !ChangeDir) vec.push_back(3);
-					  break;
-			}
-			case 1:
-			{
-					  if (map.getTM()[floor(Y) + 1][floor(X)] == 'r' ||  \
-						  map.getTM()[floor(Y) + 1][floor(X)] == 'p' && (map.getTM()[floor(Y) + 2][floor(X)] == 'p' ||\
-						  map.getTM()[floor(Y) + 2][floor(X)] == 'r' || map.getTM()[floor(Y) + 1][floor(X) + 2] == 'r' )) \
-						  vec.push_back(1);
-					  if (map.getTM()[floor(Y)][floor(X) - 1] == 'r' && map.getTM()[floor(Y) + 1][floor(X) - 1] == 'r'&& !ChangeDir) vec.push_back(2);
-					  if (map.getTM()[floor(Y)][floor(X) + 2] == 'r' && map.getTM()[floor(Y) - 1][floor(X) + 2] == 'r'&& !ChangeDir) vec.push_back(0);
-					  break;
-			}
-			case 2:
-			{
-					  if (map.getTM()[ceil(Y)][ceil(X) - 1] == 'r' || \
-						  map.getTM()[ceil(Y)][ceil(X) - 1] == 'p' && (map.getTM()[ceil(Y)][ceil(X) - 2] == 'p' || \
-						  map.getTM()[ceil(Y)][ceil(X) - 2] == 'r' || map.getTM()[ceil(Y) + 2][ceil(X) - 1] == 'r')) \
-						  vec.push_back(2);
-					  if (map.getTM()[ceil(Y) - 1][ceil(X)] == 'r' && map.getTM()[ceil(Y) - 1][ceil(X) - 1] == 'r'&& !ChangeDir) vec.push_back(3);
-					  if (map.getTM()[ceil(Y) + 2][ceil(X)] == 'r' && map.getTM()[ceil(Y) + 2][ceil(X) + 1] == 'r'&& !ChangeDir) vec.push_back(1);
-					  break;
-			}
-			case 3:
-			{
-					  if (map.getTM()[ceil(Y) - 1][ceil(X)] == 'r' || \
-						  map.getTM()[ceil(Y) - 1][ceil(X)] == 'p' && (map.getTM()[ceil(Y) - 2][ceil(X)] == 'p' || \
-						  map.getTM()[ceil(Y) - 2][ceil(X)] == 'r' || map.getTM()[ceil(Y) - 1][ceil(X) - 2] == 'r')) \
-						  vec.push_back(3);
-					  if (map.getTM()[ceil(Y)][ceil(X) + 1] == 'r' && map.getTM()[ceil(Y) - 1][ceil(X) + 1] == 'r'&& !ChangeDir) vec.push_back(0);
-					  if (map.getTM()[ceil(Y)][ceil(X) - 2] == 'r' && map.getTM()[ceil(Y) + 1][ceil(X) - 2] == 'r'&& !ChangeDir) vec.push_back(2);
-					  break;
-			}
+				  if (TLMap.getTM()[mY()][mX() + 1] == 'r' || \
+					  TLMap.getTM()[mY()][mX() + 1] == 'p' && (TLMap.getTM()[mY()][mX() + 2] == 'p' ||\
+					  TLMap.getTM()[mY()][mX() + 2] == 'r' || TLMap.getTM()[mY() - 2][mX() + 1] == 'r'))\
+					  vec.push_back(0);
+				  if (TLMap.getTM()[mY() + 1][mX()] == 'r' && TLMap.getTM()[mY() + 1][mX() + 1] == 'r' && !ChangeDir) vec.push_back(1);
+				  if (TLMap.getTM()[mY() - 2][mX()] == 'r' && TLMap.getTM()[mY() - 2][mX() - 1] == 'r'&& !ChangeDir) vec.push_back(3);
+				  break;
 		}
-	}	
+		case 1:
+		{
+				  if (TLMap.getTM()[mY() + 1][mX()] == 'r' || \
+					  TLMap.getTM()[mY() + 1][mX()] == 'p' && (TLMap.getTM()[mY() + 2][mX()] == 'p' ||\
+					  TLMap.getTM()[mY() + 2][mX()] == 'r' || TLMap.getTM()[mY() + 1][mX() + 2] == 'r'))\
+					  vec.push_back(1);
+				  if (TLMap.getTM()[mY()][mX() - 1] == 'r' && TLMap.getTM()[mY() + 1][mX() - 1] == 'r' && !ChangeDir) vec.push_back(2);
+				  if (TLMap.getTM()[mY()][mX() + 2] == 'r' && TLMap.getTM()[mY() - 1][mX() + 2] == 'r'&& !ChangeDir) vec.push_back(0);
+				  break;
+		}
+		case 2:
+		{
+				  if (TLMap.getTM()[mY()][mX() - 1] == 'r' || \
+					  TLMap.getTM()[mY()][mX() - 1] == 'p' && (TLMap.getTM()[mY()][mX() - 2] == 'p' ||\
+					  TLMap.getTM()[mY()][mX() - 2] == 'r' || TLMap.getTM()[mY() + 2][mX() - 1] == 'r'))\
+					  vec.push_back(2);
+				  if (TLMap.getTM()[mY() - 1][mX()] == 'r' && TLMap.getTM()[mY() - 1][mX() - 1] == 'r'&& !ChangeDir) vec.push_back(3);
+				  if (TLMap.getTM()[mY() + 2][mX()] == 'r' && TLMap.getTM()[mY() + 2][mX() + 1] == 'r'&& !ChangeDir) vec.push_back(1);
+				  break;
+		}
+		case 3:
+		{
+				  if (TLMap.getTM()[mY() - 1][mX()] == 'r' || \
+					  TLMap.getTM()[mY() - 1][mX()] == 'p' && (TLMap.getTM()[mY() - 2][mX()] == 'p' ||\
+					  TLMap.getTM()[mY() - 2][mX()] == 'r' || TLMap.getTM()[mY() - 1][mX() - 2] == 'r'))\
+					  vec.push_back(3);
+				  if (TLMap.getTM()[mY()][mX() + 1] == 'r' && TLMap.getTM()[mY() - 1][mX() + 1] == 'r'&& !ChangeDir) vec.push_back(0);
+				  if (TLMap.getTM()[mY()][mX() - 2] == 'r' && TLMap.getTM()[mY() + 1][mX() - 2] == 'r'&& !ChangeDir) vec.push_back(2);
+				  break;
+		}
+	}
 	return vec;
 }
 
 
 
-void Car::go(Map & map, sf::RenderWindow & window)
+bool Car::lightAround(const Map & TLMap) const
 {
-	if (Direction == 0 || Direction == 1)  
+	int koef = 1;
+	if (Direction == 1 || Direction == 3) koef = -1;
+	for (auto it = TrafficLight::AllTrafficLight.begin(); it != TrafficLight::AllTrafficLight.end(); ++it)
 	{
-		if (map.getTM()[floor(Y)][floor(X)] == 'r' && ChangeDir) ChangeDir = false;
+		if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p')
+		{
+			if (mX() + koef * mdY() == it->getX() && mY() + koef * mdX() == it->getY()\
+				&& it->getColor() == sf::Color::Red) return false;
+		}
 	}
-	if (Direction == 2 || Direction == 3) 
-	{
-		if (map.getTM()[ceil(Y)][ceil(X)] == 'r' && ChangeDir)  ChangeDir = false;
-	}
+	return true;
+}
+
+
+void Car::go(sf::RenderWindow & window, const Map &  TLMap)
+{
+	if (TLMap.getTM()[mY()][mX()] == 'r' && ChangeDir)  ChangeDir = false;
 	srand(time(0));
-	auto freeDir(freeDirections(map));
+	std::vector<int> freeDir;
+	if (lightAround(TLMap)) freeDir = freeDirections(TLMap);
 	if (freeDir.size())
 	{		
 			int random = rand() % freeDir.size();
@@ -213,16 +252,15 @@ void Car::go(Map & map, sf::RenderWindow & window)
 			{
 				if (random == i)
 				{
-					if (coordinateChange(freeDir, i))
+					if (coordinateChange(freeDir, i, TLMap))
 					{
 						X += dX;
 						Y += dY;
-						Sprite.move(120 * dX, 120 * dY);
+						Sprite.move(dX, dY);
 						Oil -= 0.00000000001;
 					}
 				}
 			}			
-	}
-	okruglenie();
-	window.draw(Sprite);	
+	}	
+	window.draw(Sprite);
 }
