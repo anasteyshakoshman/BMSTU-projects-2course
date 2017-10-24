dfcef0a4b593ca6d35f44d1f82c9f1a1fdcfd450
Анастасия <nastena.andreevna.99@gmail.com>
2017-10-24 22:36:32 +0300

    Update TrafficLight.cpp

--- KR -Model of Road Traffic/TrafficLight.cpp
+++ KR -Model of Road Traffic/TrafficLight.cpp
@@ -1,22 +1,73 @@
-#include "Include.h"
+#include "TrafficLight.h"
+#include "Map.h"
 
-#pragma once
-class Map;
+std::vector<TrafficLight> TrafficLight::AllTrafficLight;
 
-class TrafficLight
+
+TrafficLight::TrafficLight(sf::RenderWindow & window, const Map & map, const int x, const int y)
 {
-	sf::CircleShape Circle;
-	sf::Color Color;
-	float X;
-	float Y;
-public:
-	static std::vector<TrafficLight> AllTrafficLight;
+	Circle.setRadius(30);
+	X = x * 120;
+	Y = y * 120;
+	if (map.getTM()[y + 1][x + 1] == 'p')  // direction = 1
+	{
+		Color = sf::Color::Red;
+		Circle.setPosition(X + 60, Y + 60);
+	}
+	else if (map.getTM()[y + 1][x - 1] == 'p')   // direction = 2
+	{
+		Color = sf::Color::Green;
+		Circle.setPosition(X, Y + 60);
+	}
+	else if (map.getTM()[y - 1][x - 1] == 'p')   // direction = 3
+	{
+		Color = sf::Color::Red;
+		Circle.setPosition(X, Y);
+	}
+	else if (map.getTM()[y - 1][x + 1] == 'p')    // direction = 0
+	{
+		Color = sf::Color::Green;
+		Circle.setPosition(X + 60, Y);
+	}
+	Circle.setFillColor(Color);
+	window.draw(Circle);
+	AllTrafficLight.push_back(*this);
+}
 
-	TrafficLight(sf::RenderWindow &, const Map & map, const int x, const int y);
-	const sf::Color getColor();
-	~TrafficLight();
-	int getX() const;
-	int getY() const;
-	bool work(const float & clock, sf::RenderWindow & window);
-};
 
+bool TrafficLight::work(const float & clock, sf::RenderWindow & window)
+{
+	bool change = false;
+	if (clock == 3600)
+	{
+		if (Color == sf::Color::Red) Color = sf::Color::Green;
+		else Color = sf::Color::Red;
+		Circle.setFillColor(Color);
+		change = true;
+	}
+	window.draw(Circle);
+	return change;
+}
+
+int TrafficLight::getX() const
+{
+	return X;
+}
+int TrafficLight::getY() const
+{
+	return Y;
+}
+
+
+const sf::Color TrafficLight::getColor()
+{
+	return Color;
+}
+
+
+
+TrafficLight::~TrafficLight()
+{
+	X = 0;
+	Y = 0;
+}
