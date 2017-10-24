edc297ac4c93c600c819b2307ed6b127e225e17a
Анастасия <nastena.andreevna.99@gmail.com>
2017-10-24 22:32:12 +0300

    Update Car.h

--- KR -Model of Road Traffic/Car.h
+++ KR -Model of Road Traffic/Car.h
@@ -1,34 +1,40 @@
+#pragma once
 #include "Include.h"
+
 class Map;
 class Car
 {
-	double X, Y;
-	double dX, dY;
-	double Speed;
-	int Direction; 
+	//Map TLMap;
+	int X, Y;
+	int dX, dY;
+	int Speed;
+	int Direction;
 	float Oil;
 	sf::Image Image;
 	sf::Texture Texture;
 	sf::Sprite Sprite;
 	sf::Color Color;
 	bool ChangeDir;
+	
 public:
 	static std::vector<Car> AllCars;
-	/*Car(const sf::Color, Map &);*/
-	Car(const sf::Color, Map &, const double x, const double y);
-	~Car();
-	void go(Map &, sf::RenderWindow &);
-	const bool around(Map &);
-	const sf::Sprite & getSprite();
-	const std::vector<int> freeDirections(Map & map);
-	bool coordinateChange(const std::vector<int>, const int index);
-	const double getX();
-	const double getY();
-	void okruglenie();
-	void changedir();
-	const bool nextCar(const double dx, const double dy);
 
-	
-	
+	Car(const sf::Color col, const Map & map, sf::RenderWindow & window, const int x, const int y);
+	~Car();
+	void go(sf::RenderWindow &, const Map &  TLMap);
+	const std::vector<int> freeDirections(const Map &  TLMap) const;
+	bool coordinateChange(const std::vector<int>, const int index, const Map & map);
+	const int getX() const;
+	const int getY() const;
+	const int mX() const;
+	const int mY() const;
+	const int mdX() const;
+	const int mdY() const;
+	bool lightAround(const Map & TLMap) const;
+	const bool nextCar(const int dx, const int dy, const Map & TLMap) const;
+
+
+
+
 
 };
