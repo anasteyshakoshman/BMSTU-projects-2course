6f240db4145603d4afa282f93b6f2cd8672cd4d0
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-08 20:28:14 +0300

    Update Car.cpp

--- KR -Model of Road Traffic/Car.cpp
+++ KR -Model of Road Traffic/Car.cpp
@@ -9,26 +9,36 @@
 
 std::vector<Car> Car::AllCars;
 
-Car::Car(const sf::Color col, const Map &  map, sf::RenderWindow & window, const int x, const int y)
+Car::Car(const sf::Color col, const Map &  map, sf::RenderWindow & window)
 {
 	Image.loadFromFile("images/car0.png");
-	Image.createMaskFromColor(sf::Color::White);
+	Image.createMaskFromColor(sf::Color::White);   //убираем белый фон у спрайта машинки
 	Texture.loadFromImage(Image);
 	Sprite.setTexture(Texture);
 	Sprite.setColor(col);
-	Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));
-	X = x * 120;
-	Y = y * 120;
-	Speed = 0.05 * 120;
-	dX = Speed;
+	Sprite.setTextureRect(sf::IntRect(0, 0, pix, pix)); 
+	if (map.getSimvol() == 'a')
+	{
+		X = 4;
+		Y = 3;
+	}
+	else if (map.getSimvol() == 'b')
+	{
+		X = 7;
+		Y = 7;
+	}
+	X *= pix;
+	Y *= pix;
+	Speed = 0.1 * pix; 
+	dX = Speed;          
 	dY = 0;
-	Direction = 0; // 0 (+X), 1 (+Y), 2(-X), 3(-Y)
-	Oil = 100;
-	//TLMap = map;
-	ChangeDir = false;
-	Sprite.setPosition(X, Y);
-	AllCars.push_back(*this);
-	window.draw(Sprite);
+	Direction = 0; // 0 (+X), 1 (+Y), 2(-X), 3(-Y)      //   -------------------------------->  X
+	Oil = 100.;                                          //   |
+	//TLMap = map;                                      //   |                                 
+	ChangeDir = false;                                  //   |
+	Sprite.setPosition(X, Y);                           //   |
+	AllCars.push_back(*this);                           //   \/
+	window.draw(Sprite);                                // Y
 };
 
 
@@ -57,46 +67,88 @@ const int Car::getY() const
 };
 
 
-const bool Car::nextCar(const int dx, const int dy, const Map & TLMap) const
+
+const int Car::mX() const         //  (mapX) перевод координат в доступные для подстановки в карту
 {
-	int interval = 2;
-	if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p' && TLMap.getTM()[mY() + 2 * mdY()][mX() + 2 * mdX()] == 'p' /*\
-		|| TLMap.getTM()[*/ ) interval *= 10;
-	for (auto it = AllCars.begin(); it != AllCars.end(); ++it)
+	int x = X;
+	if (x % pix == 0) return x / pix;
+	else
 	{
-		if (X + dx * interval + 120 == it->getX() && Y + dy * interval + 120 == it->getY()) return false;
+		int ost = x;
+		while (ost > pix) ost -= pix;
+		if (Direction == 0 || Direction == 1) return (x - ost) / pix;   //floor    если направление положительное, округляем в меньшую сторону
+		if (Direction == 2 || Direction == 3) return (x - ost + pix) / pix;   //ceil       если направление отрицательно,  в большую
 	}
-	return true;
+};
+
+const int Car::mY() const               //тот же mX(), только для Y
+{
+	int y = Y;
+	if (y % pix == 0) return y / pix;
+	else
+	{
+		int ost = y;
+		while (ost > pix) ost -= pix;
+		if (Direction == 0 || Direction == 1) return (y - ost) / pix;   //floor
+		if (Direction == 2 || Direction == 3) return (y - ost + pix) / pix;   //ceil
+	}
+};
+
+const int Car::mdX() const      // возвращает либо 1, либо - 1, либо 0
+{          
+	return dX / Speed;
+};
+
+const int Car::mdY() const
+{
+	return dY / Speed;
+};
+
+bool Car::operator !=(const Car & other) 
+{
+	return Color == other.Color && X == other.X && Y == other.Y && Direction == other.Direction;
 };
 
 
-bool Car::coordinateChange(const std::vector<int> vec, const int index, const Map & map)
+const bool Car::nextCar(const int dx, const int dy, const Map & TLMap) const   //чтобы машинки не врезались
+{                                                                              // вызывается в след. методе chageDirection
+	int interval = 0;
+	if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p' && TLMap.getTM()[mY() + 2 * mdY()][mX() + 2 * mdX()] == 'p')  interval = 10;
+	for (auto it = AllCars.begin(); it != AllCars.end(); ++it )
+	{
+		if ((X + dx * interval + dx/Speed * pix) == it->getX() && (Y + dy * interval + dy/Speed * pix) == it->getY()) return false;
+	}
+	return true;  
+};
+
+
+bool Car::changeCoordinate(const int direct, const Map & map)   //смена направления машинки
 {
 	bool change = false;
-	if (Direction != vec[index])
+	if (Direction != direct)    
 	{
-		if (!vec[index] && nextCar(Speed, 0, map))
-		{
-			dX = Speed;
+		if (!direct && nextCar(Speed, 0, map))   //заодно идет проверка : нет ли впереди машины
+		{                                         //если есть, то данная машина не должна ехать (возвращаем change = false)
+			dX = Speed;                           // и затем в методе go не произойдеt  приращение координат (X += dX)
 			dY = 0;
 			change = true;
 			Image.loadFromFile("images/car0.png");
 		}
-		if (vec[index] == 1 && nextCar(0, Speed, map))
+		if (direct == 1 && nextCar(0, Speed, map))
 		{
 			dX = 0;
 			dY = Speed;
 			change = true;
 			Image.loadFromFile("images/car1.png");
 		}
-		if (vec[index] == 2 && nextCar(-Speed, 0, map))
+		if (direct == 2 && nextCar(-Speed, 0, map))
 		{
 			dX = -Speed;
 			dY = 0;
 			change = true;
 			Image.loadFromFile("images/car2.png");
 		}
-		if (vec[index] == 3 && nextCar(0, -Speed, map))
+		if (direct == 3 && nextCar(0, -Speed, map))
 		{
 			dX = 0;
 			dY = -Speed;
@@ -105,61 +157,26 @@ bool Car::coordinateChange(const std::vector<int> vec, const int index, const Ma
 		}
 		if (change)
 		{
-			Direction = vec[index];
-			ChangeDir = true;
-			Image.createMaskFromColor(sf::Color::White);
+			Image.createMaskFromColor(sf::Color::White);   //убираем белый фон у спрайта машинки
 			Texture.loadFromImage(Image);
 			Sprite.setTexture(Texture);
-			Sprite.setTextureRect(sf::IntRect(0, 0, 120, 120));
+			Sprite.setTextureRect(sf::IntRect(0, 0, pix, pix));
+			Direction = direct;
+			ChangeDir = true;               //если машинка поменяла направление, то она не может его еще раз поменять, пока не уедет с данного перекрестка
 		}
 		return change;
 	}	
 	else return nextCar(dX, dY, map);
 };
 
-const int Car::mX() const
-{
-	int x = X;
-	if (x % 120 == 0) return x / 120;
-	else
-	{
-		int ost = x;
-		while (ost > 120) ost -= 120;
-		if (Direction == 0 || Direction == 1) return (x - ost) / 120;   //floor
-		if (Direction == 2 || Direction == 3) return (x - ost + 120) / 120;   //ceil
-	}
-};
-
-const int Car::mY() const
-{
-	int y = Y;
-	if (y % 120 == 0) return y / 120;
-	else
-	{
-		int ost = y;
-		while (ost > 120) ost -= 120;
-		if (Direction == 0 || Direction == 1) return (y - ost) / 120;   //floor
-		if (Direction == 2 || Direction == 3) return (y - ost + 120) / 120;   //ceil
-	}
-};
-
-const int Car::mdX() const
-{
-	return dX / 6;
-};
-
-const int Car::mdY() const
-{
-	return dY / 6;
-};
 
 
 
-const std::vector<int> Car::freeDirections(const Map &  TLMap) const
-{
-	std::vector<int> vec;
 
-//	if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'r' || \
+const std::vector<int> Car::freeDirections(const Map &  TLMap) const  //возвращает вектор возможных свободных напрвлений
+{                                                                     // вызывается в методе go (в котором уже рандомно выбирается одно из свободных направлений)
+
+//	if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'r' || \                         //неудавшаяся попытка систематизировать поиск направлений и избавится от switch
 //		TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p' && \
 //(TLMap.getTM()[mY() + 2 * mdY()][mX() + 2 * mdX()] == 'p' || \
 //		TLMap.getTM()[mY() + 2 * mdY()][mX() + 2 * mdX()] == 'r' || \
@@ -174,13 +191,20 @@ const std::vector<int> Car::freeDirections(const Map &  TLMap) const
 //		TLMap.getTM()[mY() + mdY() - 2 * mdX() * pow(-1, Direction)][mX() + mdX() - 2 * mdY() * pow(-1, Direction)] == 'r'&& !ChangeDir)
 //		vec.push_back((Direction + 3) % 4);
 
-	                                   								
-	switch (Direction)                     
+
+	std::vector<int> vec;
+	/*if (X % pix != 0 && Y % pix != 0)
 	{
+		vec.push_back(Direction);
+	}
+	else
+	{*/
+		switch (Direction)
+		{
 		case 0:
 		{
 				  if (TLMap.getTM()[mY()][mX() + 1] == 'r' || \
-					  TLMap.getTM()[mY()][mX() + 1] == 'p' && (TLMap.getTM()[mY()][mX() + 2] == 'p' ||\
+					  TLMap.getTM()[mY()][mX() + 1] == 'p' && (TLMap.getTM()[mY()][mX() + 2] == 'p' || \
 					  TLMap.getTM()[mY()][mX() + 2] == 'r' || TLMap.getTM()[mY() - 2][mX() + 1] == 'r'))\
 					  vec.push_back(0);
 				  if (TLMap.getTM()[mY() + 1][mX()] == 'r' && TLMap.getTM()[mY() + 1][mX() + 1] == 'r' && !ChangeDir) vec.push_back(1);
@@ -190,7 +214,7 @@ const std::vector<int> Car::freeDirections(const Map &  TLMap) const
 		case 1:
 		{
 				  if (TLMap.getTM()[mY() + 1][mX()] == 'r' || \
-					  TLMap.getTM()[mY() + 1][mX()] == 'p' && (TLMap.getTM()[mY() + 2][mX()] == 'p' ||\
+					  TLMap.getTM()[mY() + 1][mX()] == 'p' && (TLMap.getTM()[mY() + 2][mX()] == 'p' || \
 					  TLMap.getTM()[mY() + 2][mX()] == 'r' || TLMap.getTM()[mY() + 1][mX() + 2] == 'r'))\
 					  vec.push_back(1);
 				  if (TLMap.getTM()[mY()][mX() - 1] == 'r' && TLMap.getTM()[mY() + 1][mX() - 1] == 'r' && !ChangeDir) vec.push_back(2);
@@ -200,7 +224,7 @@ const std::vector<int> Car::freeDirections(const Map &  TLMap) const
 		case 2:
 		{
 				  if (TLMap.getTM()[mY()][mX() - 1] == 'r' || \
-					  TLMap.getTM()[mY()][mX() - 1] == 'p' && (TLMap.getTM()[mY()][mX() - 2] == 'p' ||\
+					  TLMap.getTM()[mY()][mX() - 1] == 'p' && (TLMap.getTM()[mY()][mX() - 2] == 'p' || \
 					  TLMap.getTM()[mY()][mX() - 2] == 'r' || TLMap.getTM()[mY() + 2][mX() - 1] == 'r'))\
 					  vec.push_back(2);
 				  if (TLMap.getTM()[mY() - 1][mX()] == 'r' && TLMap.getTM()[mY() - 1][mX() - 1] == 'r'&& !ChangeDir) vec.push_back(3);
@@ -210,57 +234,53 @@ const std::vector<int> Car::freeDirections(const Map &  TLMap) const
 		case 3:
 		{
 				  if (TLMap.getTM()[mY() - 1][mX()] == 'r' || \
-					  TLMap.getTM()[mY() - 1][mX()] == 'p' && (TLMap.getTM()[mY() - 2][mX()] == 'p' ||\
+					  TLMap.getTM()[mY() - 1][mX()] == 'p' && (TLMap.getTM()[mY() - 2][mX()] == 'p' || \
 					  TLMap.getTM()[mY() - 2][mX()] == 'r' || TLMap.getTM()[mY() - 1][mX() - 2] == 'r'))\
 					  vec.push_back(3);
 				  if (TLMap.getTM()[mY()][mX() + 1] == 'r' && TLMap.getTM()[mY() - 1][mX() + 1] == 'r'&& !ChangeDir) vec.push_back(0);
 				  if (TLMap.getTM()[mY()][mX() - 2] == 'r' && TLMap.getTM()[mY() + 1][mX() - 2] == 'r'&& !ChangeDir) vec.push_back(2);
 				  break;
 		}
-	}
+		}
+	/*}	*/
 	return vec;
 }
 
 
 
-bool Car::lightAround(const Map & TLMap) const
-{
+bool Car::lightAround(const Map & TLMap) const      //проверка на светофоры
+{                                              
 	int koef = 1;
 	if (Direction == 1 || Direction == 3) koef = -1;
-	for (auto it = TrafficLight::AllTrafficLight.begin(); it != TrafficLight::AllTrafficLight.end(); ++it)
+	
+	if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p')   //чтобы реагировала только на светофор, предназначающийся ей (т.е., стоящий перед перекрестком)
 	{
-		if (TLMap.getTM()[mY() + mdY()][mX() + mdX()] == 'p')
+		for (auto it = TrafficLight::AllTrafficLight.begin(); it != TrafficLight::AllTrafficLight.end(); ++it)
 		{
-			if (mX() + koef * mdY() == it->getX() && mY() + koef * mdX() == it->getY()\
-				&& it->getColor() == sf::Color::Red) return false;
-		}
-	}
+			if (mX() + koef * mdY() == it->getX()  && mY() + koef * mdX() == it->getY() && it->getColor() == sf::Color::Red) 
+				return false;
+		}  
+	}	
 	return true;
 }
 
 
-void Car::go(sf::RenderWindow & window, const Map &  TLMap)
+void Car::go(sf::RenderWindow & window, const Map &  TLMap)         //основной метод движения 
 {
-	if (TLMap.getTM()[mY()][mX()] == 'r' && ChangeDir)  ChangeDir = false;
-	srand(time(0));
+	if (TLMap.getTM()[mY()][mX()] == 'r' && ChangeDir)  ChangeDir = false;      // как только машинка уехала с перекрестка, на котором поменяла направление
+	srand(time(0));                                                            // (т.е. выехала на 'r'), она снова может его менять
 	std::vector<int> freeDir;
-	if (lightAround(TLMap)) freeDir = freeDirections(TLMap);
+	if (lightAround(TLMap)) freeDir = freeDirections(TLMap);     //проверяем светофор, если есть и красный, то freedir будет пустым, и приращение X (Y) не произойдет
 	if (freeDir.size())
 	{		
-			int random = rand() % freeDir.size();
-			for (int i = 0; i < freeDir.size(); ++i)
+			int random = rand() % freeDir.size();          //random имеет диапозон от 0 до кол-ва свободных направлений
+			if (changeCoordinate(freeDir[random], TLMap))     //движемся, если впереди нет машины
 			{
-				if (random == i)
-				{
-					if (coordinateChange(freeDir, i, TLMap))
-					{
 						X += dX;
 						Y += dY;
 						Sprite.move(dX, dY);
 						Oil -= 0.00000000001;
-					}
-				}
-			}			
+			}
 	}	
-	window.draw(Sprite);
+	window.draw(Sprite);     // вне зависимости от того, двигалась ли машина, рисуем ее
 }
