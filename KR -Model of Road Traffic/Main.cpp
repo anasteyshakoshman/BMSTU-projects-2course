eb197fb31d4452d6f8d45dcea6d491129b5a5f06
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-08 20:30:09 +0300

    Update Main.cpp

--- KR -Model of Road Traffic/Main.cpp
+++ KR -Model of Road Traffic/Main.cpp
@@ -3,11 +3,10 @@
 #include "Map.h"
 #include "View.h"
 #include "TrafficLight.h"
-#include <stdlib.h>
+#include <ctime>
 
 
-
-void createTrafficLight(sf::RenderWindow & window, const Map & map)
+void createTrafficLight(sf::RenderWindow & window, const Map & map)        //создание светофоров
 {
 	for (int i = 0; i < map.getHeight(); ++i)
 	{
@@ -18,73 +17,55 @@ void createTrafficLight(sf::RenderWindow & window, const Map & map)
 	}
 }
 
-
-void addCar(sf::RenderWindow & window, const Map & map)
+void addCar(sf::RenderWindow & window, const Map & map, std::pair <int, int> & currentClock)   //функция добавления машинки
 {
-	std::string tmp;
-	sf::Color color;
-	bool enter = false;
-	while (!enter)
+	if (currentClock.first - currentClock.second >= 500 && sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 	{
-		std::cout << "Enter color of your car : ";
-		std::cin >> tmp;
-		if (tmp == "Green" || tmp == "green")
-		{
-			color = sf::Color::Green;
-			enter = true;
-		}
-		else if (tmp == "Black" || tmp == "black")
-		{
-			color = sf::Color::Black;
-			enter = true;
-		}
-		else if (tmp == "Yellow" || tmp == "yellow")
-		{
-			color = sf::Color::Yellow;
-			enter = true;
-		}
-		else if (tmp == "Red" || tmp == "red")
-		{
-			color = sf::Color::Red;
-			enter = true;
-		}
-		else if (tmp == "Blue" || tmp == "blue")
-		{
-			color = sf::Color::Blue;
-			enter = true;
-		}
-	} 
-	Car(color, map, window, 7, 6);
+		currentClock.second = currentClock.first;
+		srand(time(0));
+		int random = rand() % 4;
+		sf::Color color;
+		if (!random) color = sf::Color::Green;
+		else if (random == 2) color = sf::Color::Yellow;
+		else if (random == 3) color = sf::Color::Red;
+		else if (random == 1) color = sf::Color::Blue;
+		Car(color, map, window);
+	}
 }
 
 
 
-void Cars(const Map & map, sf::RenderWindow & window )
+
+void Cars(const Map & map, sf::RenderWindow & window )   
 {
-	Car (sf::Color::Blue, map, window, 8, 7);
-	Car (sf::Color::Green, map, window, 9, 7);
-	Car (sf::Color::Red, map, window, 10, 7);
-	Car (sf::Color::Yellow, map, window, 11, 7);
-	Car (sf::Color::Black, map, window, 12, 7);
-	Car (sf::Color::Transparent, map, window, 13, 7);
+	Car (sf::Color::Blue, map, window);
+	Car (sf::Color::Green, map, window);
+	Car (sf::Color::Red, map, window);
+	Car (sf::Color::Yellow, map, window);
+	Car (sf::Color::Black, map, window);
+	Car (sf::Color::Transparent, map, window);
 } 
+
 
 void main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Cars"/*, sf::Style::Fullscreen*/);   //окно
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Cars", sf::Style::Fullscreen);   //окно
 	View view;   //камера
-	Map map("map.png");   //карта
-	Car car1(sf::Color::Red, map, window, 10, 7);
-	Car car(sf::Color::Blue, map, window, 8, 7);
-	//Cars(map, window);
+	Map map("map.png", 'a');   //карта
+
+	size_t numCar = 1;  
+
+	Car car(sf::Color::Blue, map, window);
+
 	sf::Clock clock;    //часы
-	float currentClock = 0;     //текущее время
-	createTrafficLight(window, map);
+	std::pair <int, int> currentClock;     //first - текущее время, second - переменная для addCar
+	currentClock.first = 0;
+	createTrafficLight(window, map);  //создание светофоров
 	while (window.isOpen())     
 	{
 		sf::Event event;
 		float time = clock.getElapsedTime().asMicroseconds();
-		clock.restart();   //запускаем часы
+		clock.restart();   //запуск часов
 		time = time / 800;
 		while (window.pollEvent(event))
 		{
@@ -94,27 +75,33 @@ void main()
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
 		{
-			std::cout << "";
+			std::cout << 1;//для Local  Windows Debugger (ставлю keyboard)
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-		{
-			addCar(window, map);
-		}
-		view.work(window, time);  //включаем камеру
+		view.work(window, time);  //включаю камеру
 		window.clear();
-		currentClock += 1;
-		map.draw(window);   //рисуем карту
-		for (auto it = Car::AllCars.begin(); it != Car::AllCars.end(); ++it)
+		++ currentClock.first;
+		addCar(window, map, currentClock);
+		map.draw(window);   //рисую карту
+		
+		for (auto it = Car::AllCars.begin(); it != Car::AllCars.end(); ++it)   //запус движения машинок
 		{
 			it->go(window, map);
 		}
-		for (auto it = TrafficLight::AllTrafficLight.begin(); it != TrafficLight::AllTrafficLight.end(); ++it)
+		for (auto it = TrafficLight::AllTrafficLight.begin(); it != TrafficLight::AllTrafficLight.end(); ++it) //запуск работы светофоров
 		{
-			if (it->work(currentClock, window))
-				currentClock = 0;
+			it->work(currentClock.first, window);
 		}
-		window.display();
-	}	
+		if (numCar < Car::AllCars.size())
+		{
+			numCar = Car::AllCars.size();
+			std::cout << numCar;
+		}
+		
+
+
+
+		window.display();               //вывод в окно
+	}	 
 }
 
 
