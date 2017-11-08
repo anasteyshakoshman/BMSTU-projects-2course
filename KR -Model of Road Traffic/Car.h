625e40d5cd2893f5326ad54e4c5438a7a6482dfc
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-08 20:29:18 +0300

    Update Car.h

--- KR -Model of Road Traffic/Car.h
+++ KR -Model of Road Traffic/Car.h
@@ -5,25 +5,25 @@ class Map;
 class Car
 {
 	//Map TLMap;
-	int X, Y;
-	int dX, dY;
-	int Speed;
+	int X, Y;      //координаты типа int, так как y типа float число записывалось с каким-то непонятным приращением
+	int dX, dY;    // пример : я писала float x = 0.2 (оно записывалосось как 0.200000009), при увелечении координат это приращение увеличивалось и влияло координаты
+	int Speed;     
 	int Direction;
-	float Oil;
+	double Oil;
 	sf::Image Image;
 	sf::Texture Texture;
-	sf::Sprite Sprite;
+	sf::Sprite Sprite;              
 	sf::Color Color;
-	bool ChangeDir;
+	bool ChangeDir;     //переменная, для контроля за тем, чтобы машинка не могла менять напрвление на перекрестке по 10 раз 
 	
 public:
 	static std::vector<Car> AllCars;
 
-	Car(const sf::Color col, const Map & map, sf::RenderWindow & window, const int x, const int y);
+	Car(const sf::Color col, const Map & map, sf::RenderWindow & window);
 	~Car();
 	void go(sf::RenderWindow &, const Map &  TLMap);
 	const std::vector<int> freeDirections(const Map &  TLMap) const;
-	bool coordinateChange(const std::vector<int>, const int index, const Map & map);
+	bool changeCoordinate(const int direct, const Map & map);
 	const int getX() const;
 	const int getY() const;
 	const int mX() const;
@@ -32,6 +32,7 @@ public:
 	const int mdY() const;
 	bool lightAround(const Map & TLMap) const;
 	const bool nextCar(const int dx, const int dy, const Map & TLMap) const;
+	bool operator !=(const Car & other);
 
 
 
