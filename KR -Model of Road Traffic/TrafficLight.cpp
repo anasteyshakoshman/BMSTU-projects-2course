6f6b10a6d060888278103f8b45e21cb79a787189
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-08 20:36:02 +0300

    Update TrafficLight.cpp

--- KR -Model of Road Traffic/TrafficLight.cpp
+++ KR -Model of Road Traffic/TrafficLight.cpp
@@ -6,60 +6,65 @@ std::vector<TrafficLight> TrafficLight::AllTrafficLight;
 
 TrafficLight::TrafficLight(sf::RenderWindow & window, const Map & map, const int x, const int y)
 {
-	Circle.setRadius(30);
-	X = x * 120;
-	Y = y * 120;
+	BlackCircle.setRadius(pix / 4); //черная оконтовка
+	Circle.setRadius(pix / 4 - 5);  //чтобы светофоры были меньше машин
+	X = x * pix;
+	Y = y * pix;
 	if (map.getTM()[y + 1][x + 1] == 'p')  // direction = 1
 	{
-		Color = sf::Color::Red;
-		Circle.setPosition(X + 60, Y + 60);
+	    Color = sf::Color::Red;                             //изначально присваиваю разные цвета противополжным светофорам, затем просто их меняю
+		BlackCircle.setPosition(X + pix / 2, Y + pix / 2);
+		Circle.setPosition(X + pix/2 + 5, Y + pix/2 + 5);
 	}
 	else if (map.getTM()[y + 1][x - 1] == 'p')   // direction = 2
 	{
-		Color = sf::Color::Green;
-		Circle.setPosition(X, Y + 60);
+		Color = sf::Color::Green;                             	
+		BlackCircle.setPosition(X, Y + pix / 2);
+		Circle.setPosition(X + 5, Y + pix / 2 + 5);
 	}
 	else if (map.getTM()[y - 1][x - 1] == 'p')   // direction = 3
 	{
-		Color = sf::Color::Red;
-		Circle.setPosition(X, Y);
+		Color = sf::Color::Red;                             
+		BlackCircle.setPosition(X , Y );
+		Circle.setPosition(X + 5, Y + 5);
 	}
 	else if (map.getTM()[y - 1][x + 1] == 'p')    // direction = 0
 	{
-		Color = sf::Color::Green;
-		Circle.setPosition(X + 60, Y);
+		Color = sf::Color::Green;                             
+		BlackCircle.setPosition(X + pix / 2, Y);
+		Circle.setPosition(X + pix/2 + 5, Y + 5);
 	}
+	BlackCircle.setFillColor(sf::Color::Black);
 	Circle.setFillColor(Color);
+	window.draw(BlackCircle);
 	window.draw(Circle);
 	AllTrafficLight.push_back(*this);
 }
 
 
-bool TrafficLight::work(const float & clock, sf::RenderWindow & window)
+void TrafficLight::work(const int  clock, sf::RenderWindow & window)
 {
-	bool change = false;
-	if (clock == 3600)
+	if (clock % 500 == 0)     //цвет светофоров меняется каждые 1000 microseconds 
 	{
 		if (Color == sf::Color::Red) Color = sf::Color::Green;
 		else Color = sf::Color::Red;
 		Circle.setFillColor(Color);
-		change = true;
 	}
+	window.draw(BlackCircle);
 	window.draw(Circle);
-	return change;
 }
 
 int TrafficLight::getX() const
 {
-	return X;
+	return X / pix;
 }
 int TrafficLight::getY() const
 {
-	return Y;
+	return Y / pix;
 }
 
 
-const sf::Color TrafficLight::getColor()
+sf::Color TrafficLight::getColor() const
 {
 	return Color;
 }
