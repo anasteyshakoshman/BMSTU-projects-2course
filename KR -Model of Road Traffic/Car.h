8890057e2ad5cf9f490931cc70fb638b8a475b49
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-12 08:42:22 +0300

    Update Car.h

--- KR -Model of Road Traffic/Car.h
+++ KR -Model of Road Traffic/Car.h
@@ -4,13 +4,11 @@
 class Map;
 class Car
 {
-	//Map TLMap;
 	int X, Y;      //координаты типа int, так как y типа float число записывалось с каким-то непонятным приращением
 	int dX, dY;    // пример : я писала float x = 0.2 (оно записывалосось как 0.200000009), при увелечении координат это приращение увеличивалось и влияло координаты
 	int Speed;     
 	int Direction;
 	double Oil;
-	sf::Image Image;
 	sf::Texture Texture;
 	sf::Sprite Sprite;              
 	sf::Color Color;
@@ -20,20 +18,21 @@ public:
 	static std::vector<Car> AllCars;
 
 	Car(const sf::Color col, const Map & map, sf::RenderWindow & window);
+	Car(const sf::Color col, const Map & map, sf::RenderWindow & window, const int x, const int y, const int direction);
 	~Car();
-	void go(sf::RenderWindow &, const Map &  TLMap);
-	const std::vector<int> freeDirections(const Map &  TLMap) const;
-	bool changeCoordinate(const int direct, const Map & map);
-	const int getX() const;
-	const int getY() const;
-	const int mX() const;
-	const int mY() const;
-	const int mdX() const;
-	const int mdY() const;
-	bool lightAround(const Map & TLMap) const;
-	const bool nextCar(const int dx, const int dy, const Map & TLMap) const;
-	bool operator !=(const Car & other);
-
+	void go(sf::RenderWindow &, const Map &  map);
+	std::vector<int> freeDirections(const Map &  map) const;
+	void changeDirection(const int direct, const Map & map);
+	int getX() const;
+	int getY() const;
+	int mX() const;
+	int mY() const;
+	int mdX() const;
+	int mdY() const;
+	bool lightAround(const Map & map) const;
+	bool nextCar(const Map & map) const;
+	int mod(const int num) const;
+	int getSpeed() const;
 
 
 
