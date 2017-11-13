03f524305e16b2d262c9a32e3e92725f424be8ff
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-13 03:41:11 +0300

    Update Map.cpp

--- KR -Model of Road Traffic/Map.cpp
+++ KR -Model of Road Traffic/Map.cpp
@@ -1,4 +1,5 @@
 #include "Map.h"
+#include "RoadSign.h"
 
 Map::Map(const sf::String file, const char simvol)
 {
@@ -34,55 +35,85 @@ Map::Map(const sf::String file, const char simvol)
 		TileMap =
 		{
 			"00000000000000000000000000000000000000000000000000000000000000000000000000000000",
-			"0                 s             s        s     s                        s      0",
+			"0                                                                              0",
 			"0 pprrrrrrrrrrrrpprrrrrrrrrrrrpprrrrrrrpprrrrpprrrrrrrrrrrrrrrrrrrrrrrpprrrrpp 0",
 			"0 pprrrrrrrrrrrrpprrrrrrrrrrrrpprrrrrrrpprrrrpprrrrrrrrrrrrrrrrrrrrrrrpprrrrpp 0",
-			"0 rr           srrs          srrs     srrs  srrs                     srrs   rr 0",
-			"0 rr           srrs     s     rr       rr    rr   hlpprrrrrrrrrrpp    rr    rr 0",
+			"0 rr            rr            rr       rr    rr                       rr    rr 0",
+			"0 rr           srrs           rr       rr    rr   hlpprrrrrrrrrrpp    rr    rr 0",
 			"0 rr hl pprrrrrrpprrrrpprrrrrrpp  hl   pprrrrpp   mnpprrrrrrrrrrpp    rr    rr 0",
 			"0 rr mnrpprrrrrrpprrrrpprrrrrrpp  mn   pprrrrpp     rr          rr    rr    rr 0",
-			"0 rr    rr     srrs  srrs     rr             rr     rr         srrs  srrs  srr 0",
+			"0 rr    rr     srrs   rr      rr             rr     rr         srrs   rr    rr 0",
 			"0 rr    rr      rr    rr      pprrrrrrpp     rr     pprrrrrrrrrrpprrrrpprrrrpp 0",
 			"0 rr    rr      rr    rr      pprrrrrrpp     rr     pprrrrrrrrrrpprrrrpprrrrpp 0",
-			"0 rrs   rr      rr    rr  hl          rr     rr     hl         srrs  s     srrs0",
-			"0 pprrrrpp     srrs  srr  mn         srrs   srrs    mn   s      rr   hl     rr 0",
+			"0 rr    rr      rr    rr  hl          rr     rr     hl         srrs         rr 0",
+			"0 pprrrrpp      rr    rr  mn         srrs    rr     mn          rr   hl     rr 0",
 			"0 pprrrrpp  pprrpprrrrpp       pprrrrrpprrrrrpprrrrrrrrpprrrrrrrpp   mn     rr 0",
-			"0 rrs       pprrpprrrrpp       pprrrrrpprrrrrpprrrrrrrrpprrrrrrrpp          rr 0",
-			"0 rr        rr s     srrs      rr    srrs   s      hl srrs                  rr 0",
-			"0srrs      srrs      srrs     srr     rr           mn  rr  pprrrrrrpp   hl  rr 0",
+			"0 rr        pprrpprrrrpp       pprrrrrpprrrrrpprrrrrrrrpprrrrrrrpp          rr 0",
+			"0 rr        rr        rr       rr    srrs          hl  rr                   rr 0",
+			"0 rr       srrs      srrs      rr     rr           mn  rr  pprrrrrrpp   hl  rr 0",
 			"0 pprrrrrrrrpprrrrrrrrpprrrrrrrpp     pprrrrrrpprrrrrrrpp  pprrrrrrpp   mn  rr 0",
 			"0 pprrrrrrrrpprrrrrrrrpprrrrrrrpp     pprrrrrrpprrrrrrrpp  rr      rr       rr 0",
-			"0 rrs      srrs      srr      srrs            rr           rr     srrs     srr 0",
-			"0srrs      srrs   s  srrs    s rr         hl  rr           pprrrrrrpprrrrrrrpp 0",
+			"0 rr       srrs      srrs      rr             rr           rr     srrs      rr 0",
+			"0 rr        rr        rr       rr         hl  rr           pprrrrrrpprrrrrrrpp 0",
 			"0 pprrrrrrrrpprrpprrrrpprrrpprrpp         mn  rr           pprrrrrrpprrrrrrrpp 0",
-			"0 pprrrrrrrrpprrpprrrrpprrrpprrpp            srrs       s         srrs     srrs0",
-			"0 rrs      s   srrs  s    srrs     hl pprrrrrrpprrrrrrpprrrpp      rr       rr 0",
+			"0 pprrrrrrrrpprrpprrrrpprrrpprrpp            srrs                 srrs      rr 0",
+			"0 rr            rr         rr      hl pprrrrrrpprrrrrrpprrrpp      rr       rr 0",
 			"0 rr    hl      rr         rr      mn pprrrrrrpprrrrrrpprrrpp      rr       rr 0",
-			"0 rr    mn      rr         rr        srr     srrs    srrs  rr      rr       rr 0",
-			"0srrs           rr         pprrrrrrrrrpp      rr      rr   pprrrrrrpp       rr 0",
+			"0 rr    mn      rr         rr         rr     srrs     rr   rr      rr       rr 0",
+			"0 rr            rr         pprrrrrrrrrpp      rr      rr   pprrrrrrpp       rr 0",
 			"0 pprrrrrrrrrrrrpp         pprrrrrrrrrpp      rr      rr   pprrrrrrpp       rr 0",
-			"0rpprrrrrrrrrrrrpp         rr        srrs     rr     srrs                  srr 0",
-			"0 rrs           rr         rr         rr      rr      pprrrrrrrrrrrrrrrrrrrrpp 0",
+			"0rpprrrrrrrrrrrrpp         rr         rr      rr      rr                    rr 0",
 			"0 rr            rr         rr         rr      rr      pprrrrrrrrrrrrrrrrrrrrpp 0",
-			"0 rr           srrs     s  rr        srrs    srrs     rrs             hl   srrs0",
+			"0 rr            rr         rr         rr      rr      pprrrrrrrrrrrrrrrrrrrrpp 0",
+			"0 rr            rr         rr        srrs     rr      rr              hl    rr 0",
 			"0 rr        pprrpprrrrpprrrpp  pprrrrrpprrrrrrpp      rr              mn    rr 0",
 			"0 rr   hl   pprrpprrrrpprrrpp  pprrrrrpprrrrrrpp      rr                    rr 0",
-			"0 rr   mn   rr s     srrs      rr    srrs    srrs    srrs                   rr 0",
-			"0srrs      srr        rr       rr     rr      rr      pprrrrrrrrrrrpp   hl  rr 0",
+			"0 rr   mn   rr        rr       rr    srrs     rr      rr                    rr 0",
+			"0 rr        rr        rr       rr     rr      rr      pprrrrrrrrrrrpp   hl  rr 0",
 			"0 pprrrrrrrrpp        rr       rr     rr      rr      pprrrrrrrrrrrpp   mn  rr 0",
-			"0 pprrrrrrrrpp       srrs     srr     rr      rr      rrs          rr       rr 0",
-			"0 rrs      srrs  pprrrpprrrrrrrpp    srrs    srrs    srrs     s   srrs     srr 0",
-			"0srrs      srr   pprrrpprrrrrrrpp     pprrrrrrpprrrrrrpprrrrpprrrrrpprrrrrrrpp 0",
-			"0 pprrrrrrrrpp   rr  s        srrs    pprrrrrrpprrrrrrpprrrrpprrrrrpprrrrrrrpp 0",
-			"0 pprrrrrrrrpp   rr       hl   rr     rrs    srrs    srrs  srrs   s        srrs0",
-			"0 rrs      srrs  rr       mn   rr     rr      rr      rr    rr       hl     rr 0",
-			"0 rr       srrs srrs          srrs   srrs    srrs    srrs  srrs      mn     rr 0",
+			"0 pprrrrrrrrpp        rr       rr     rr      rr      rr           rr       rr 0",
+			"0 rr        rr   pprrrpprrrrrrrpp     rr     srrs    srrs          rr       rr 0",
+			"0 rr        rr   pprrrpprrrrrrrpp     pprrrrrrpprrrrrrpprrrrpprrrrrpprrrrrrrpp 0",
+			"0 pprrrrrrrrpp   rr            rr     pprrrrrrpprrrrrrpprrrrpprrrrrpprrrrrrrpp 0",
+			"0 pprrrrrrrrpp   rr       hl   rr     rr     srrs    srrs   rr              rr 0",
+			"0 rr        rr   rr       mn   rr     rr      rr      rr    rr       hl     rr 0",
+			"0 rr        rr   rr            rr     rr      rr      rr    rr       mn     rr 0",
 			"0 pprrrrrrrrpprrrpprrrrrrrrrrrrpprrrrrpprrrrrrpprrrrrrpprrrrpprrrrrrrrrrrrrrpp 0",
 			"0 pprrrrrrrrpprrrpprrrrrrrrrrrrpprrrrrpprrrrrrpprrrrrrpprrrrpprrrrrrrrrrrrrrpp 0",
-			"0          s    s                s   s       s       s     s                   0",
+			"0                                                                              0",
 			"00000000000000000000000000000000000000000000000000000000000000000000000000000000",
 		};
 
+	}
+	else if (simvol == 'd')
+	{
+		Weight = 26;
+		Height = 21;
+		TileMap =
+		{
+			"00000000000000000000000000",
+			"0      y            k    0",
+			"0 pprrrrrrpprrrrrrrrrrpp 0",
+			"0rpprrrrrrpprrrrrrrrrrpp 0",
+			"0 rr  y   rr       k  rr 0",
+			"0 rr     srrs    y    rr 0",
+			"0 pprrrrrrpprrrrrrrrrrpp 0",
+			"0 pprrrrrrpprrrrrrrrrrpp 0",
+			"0 rr  y  srrs     y   rr 0",
+			"0 rr   y  rr      k   rr 0",
+			"0 pprrrrrrpprrpprrrrrrpp 0",
+			"0rpprrrrrrpprrpprrrrrrpp 0",
+			"0 rr          rr   n  rr 0",
+			"0 rr    k    srrs     rr 0",
+			"0 pprrrrrrrrrrpprrrrrrpp 0",
+			"0 pprrrrrrrrrrpprrrrrrpp 0",
+			"0 rr    y    srrs     rr 0",
+			"0 rr      y   rr  k   rr 0",
+			"0 pprrrrrrrrrrpprrrrrrpp 0",
+			"0rpprrrrrrrrrrpprrrrrrpp 0",
+			"0     k             y    0",
+			"00000000000000000000000000"
+		};
 	}
 	else if (simvol == 'c')
 	{
@@ -91,16 +122,16 @@ Map::Map(const sf::String file, const char simvol)
 		TileMap =
 		{
 			"00000000000000000000000000",
-			"0                        0",
+			"0         y              0",
 			"0 pprrrrrrrrrrrrrrrrrrpp 0",
 			"0rpprrrrrrrrrrrrrrrrrrpp 0",
-			"0 rr                  rr 0",
-			"0 rr                  rr 0",
-			"0 rr                  rr 0",
-			"0 rr                  rr 0",
+			"0 rr       y          rr 0",
+			"0krr                  rrk0",
+			"0 rrk                krr 0",
+			"0 rr       y          rr 0",
 			"0 pprrrrrrrrrrrrrrrrrrpp 0",
 			"0rpprrrrrrrrrrrrrrrrrrpp 0",
-			"0                        0",
+			"0             y          0",
 			"00000000000000000000000000"
 		};
 	}
@@ -183,10 +214,19 @@ void Map::draw(sf::RenderWindow & window)   //заполнение карты в
 						Sprite.setTextureRect(sf::IntRect(4 * pix, 2 * pix, pix, pix));
 						break;
 			}
-			default:
+			case ' ':
 			{
 						Sprite.setTextureRect(sf::IntRect(2 * pix, 0, pix, pix));
 						break;
+			}
+			case 's':
+			{
+						Sprite.setTextureRect(sf::IntRect(2 * pix, 0, pix, pix));
+						break;
+			}
+			default:
+			{
+					   CreateRoadSign(i, j);
 			}
 			}
 			Sprite.setPosition(j * pix, i * pix);
@@ -198,6 +238,28 @@ void Map::draw(sf::RenderWindow & window)   //заполнение карты в
 sf::Sprite Map::getSprite()
 {
 	return Sprite;
+}
+
+void Map::CreateRoadSign(const int y, const int x)
+{
+	int poz_x = 5 * pix, poz_y = 0, limit = 60;
+	if (x <= Weight - 2)  //direction = 1
+	{
+		if (TileMap[y][x + 1] == 'r' && TileMap[y][x + 2] == 'r')
+			poz_x += pix;
+	}
+	if (y <= Height - 2)   //direction = 2
+	{
+		if (TileMap[y + 1][x] == 'r' && TileMap[y + 2][x] == 'r')
+			poz_x += 2 * pix;
+	}
+	if (x >= 2)                                                          //direction = 3
+	{
+		if (TileMap[y][x - 1] == 'r' && TileMap[y][x - 2] == 'r')
+			poz_x += 3 * pix;
+	}
+	if (TileMap[y][x] == 'y') poz_y += pix;
+	Sprite.setTextureRect(sf::IntRect(poz_x, poz_y, pix, pix));
 }
 
 Map & Map::operator =(const Map & other)
