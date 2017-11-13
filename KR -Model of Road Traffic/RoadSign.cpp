8be98a44e3460aa4f89435f1644131b1f600597c
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-13 03:45:23 +0300

    Create RoadSign.cpp

---
+++ KR -Model of Road Traffic/RoadSign.cpp
@@ -0,0 +1,34 @@
+#include "RoadSign.h"
+
+ std::vector<RoadSign> RoadSign::AllRoadSign;
+
+
+RoadSign::RoadSign(const int x, const int y, const char simvol)
+{
+	X = x;
+	Y = y;
+	if(simvol == 'y') Limit = 6;
+	else Limit = 3;
+	AllRoadSign.push_back(*this);
+}
+
+
+RoadSign::~RoadSign()
+{
+	X = 0;
+	Y = 0;
+	Limit = 0;
+}
+
+int RoadSign::getLimit() const
+{
+	return Limit;
+}
+int RoadSign::getX() const
+{
+	return X;
+}
+int RoadSign::getY() const
+{
+	return Y;
+}
