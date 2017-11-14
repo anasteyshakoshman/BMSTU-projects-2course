5ab44d412cc52f550ff26924b1eeb5ce23c04a4f
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-14 03:50:01 +0300

    Update Main.cpp

--- KR -Model of Road Traffic/Main.cpp
+++ KR -Model of Road Traffic/Main.cpp
@@ -1,59 +1,18 @@
 #include "FunctionForMain.h"
 
-
-
-
 void main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Cars", sf::Style::Fullscreen);   //окно
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Model of ROAD TRAFFIC", sf::Style::Fullscreen);   //окно
 	View view;   //камера
-	Map map("map.png", 'd');   //карта  
+	Map map("map.png", 'd');   //карта 
 
-	Car car(sf::Color::Cyan, map, window);
-	Car car0(sf::Color::Magenta, map, window);
-	Car car1(sf::Color::Magenta, map, window);
-	Car car2(sf::Color::White, map, window);
-	Car car3(sf::Color::Blue, map, window);
-	Car car5(sf::Color::Green, map, window);
-	Car car6(sf::Color::Red, map, window);
-	Car car7(sf::Color::Magenta, map, window);
-	Car car8(sf::Color::Cyan, map, window);
-	Car car9(sf::Color::Red, map, window);
-	Car car10(sf::Color::Magenta, map, window);
-	Car car11(sf::Color::White, map, window);
-	Car car12(sf::Color::Blue, map, window);
-	Car car13(sf::Color::Green, map, window);
-	Car car14(sf::Color::Red, map, window);
-	Car car15(sf::Color::Blue, map, window);
-	Car car16(sf::Color::Black, map, window);
-	Car car17(sf::Color::Green, map, window);
-	Car car18(sf::Color::Cyan, map, window);
-	Car car19(sf::Color::Red, map, window);
-	Car car20(sf::Color::Magenta, map, window);
-	Car car21(sf::Color::White, map, window);
-	Car car22(sf::Color::Blue, map, window);
-	Car car23(sf::Color::Green, map, window);
-	Car car24(sf::Color::Red, map, window);
-	Car car25(sf::Color::Blue, map, window);
-	Car car26(sf::Color::Black, map, window);
-	Car car27(sf::Color::Green, map, window);
-	Car car28(sf::Color::Cyan, map, window);
-	Car car29(sf::Color::Red, map, window);
-	Car car30(sf::Color::Magenta, map, window);
-	Car car31(sf::Color::White, map, window);
-	Car car32(sf::Color::Blue, map, window);
-	Car car33(sf::Color::Green, map, window);
-	Car car34(sf::Color::Red, map, window);
-	Car car35(sf::Color::Blue, map, window);
-	Car car36(sf::Color::Black, map, window);
-	Car car37(sf::Color::Green, map, window);
-
+	sf::Texture texture;
+	texture.loadFromFile("images/car.png");
 
 	sf::Clock clock;    //часы
 	std::pair <int, int> currentClock(0, 0);    //first - текущее время, second - переменная для addCar          
 	float time = 0.;
-
-	int num = 2;
+	bool crash = false;
 
 	createTrLightAndRSign(window, map);  //создание светофоров
 
@@ -68,7 +27,7 @@ void main()
 			if (event.type == sf::Event::Closed) window.close();
 		}
 		window.clear();
-		Work(window, map, view, currentClock, time, num);
+		Work(window, map, view, texture, currentClock, time, crash);
 		window.display();               //вывод в окно
 	}	 
 }
